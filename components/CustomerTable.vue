fcdb0017d14d1f32d951c213ffce579e1e42bbde
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 17:23:51 +0300

    expose customer table component

---
+++ components/CustomerTable.vue
@@ -0,0 +1,3 @@
+<template><CustomerTableImpl v-bind="$attrs" @open="$emit('open', $event)" /></template>
+<script setup lang="ts">import CustomerTableImpl from './admin/CustomerTable.vue'
+defineEmits<{ open: [tenant: any] }>()</script>
