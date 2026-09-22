0280b67acb3aeda11402a7fcfd827f0f8921893d
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 16:11:38 +0300

    feat: add initial PKTakip landing page

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex align-items-center justify-content-center min-h-screen p-5">
+    <div class="text-center">
+      <div class="text-5xl font-bold mb-2">
+        <span style="color: var(--pk-yellow);">PK</span>Takip
+      </div>
+      <p class="text-color-secondary mt-0">Periyodik Kontrol Takip</p>
+      <Button label="Başlayalım" icon="pi pi-arrow-right" iconPos="right" class="pk-primary mt-4" />
+    </div>
+  </main>
+</template>
