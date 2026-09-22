edd000f08873df191e89fa9b559832d850c290fa
okansari85 <sari_okan@hotmail.com>
2026-09-22 16:19:15 +0300

    asdasd

---
+++ .nuxt/types/runtime-config.d.ts
@@ -0,0 +1,76 @@
+import { RuntimeConfig as UserRuntimeConfig, PublicRuntimeConfig as UserPublicRuntimeConfig } from 'nuxt/schema'
+  interface SharedRuntimeConfig {
+   app: {
+      buildId: string,
+
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+  }
+  interface SharedPublicRuntimeConfig {
+   apiBaseUrl: string,
+
+   primevue: {
+      usePrimeVue: boolean,
+
+      autoImport: boolean,
+
+      resolvePath: any,
+
+      importPT: any,
+
+      importTheme: any,
+
+      loadStyles: boolean,
+
+      options: {
+         ripple: boolean,
+
+         inputVariant: string,
+      },
+
+      components: Array<{
+
+      }>,
+
+      directives: Array<{
+
+      }>,
+
+      composables: Array<{
+
+      }>,
+
+      config: Array<{
+
+      }>,
+
+      services: Array<{
+
+      }>,
+
+      styles: Array<{
+
+      }>,
+
+      injectStylesAsString: Array<any>,
+
+      injectStylesAsStringToTop: Array<string>,
+   },
+  }
+declare module '@nuxt/schema' {
+  interface RuntimeConfig extends UserRuntimeConfig {}
+  interface PublicRuntimeConfig extends UserPublicRuntimeConfig {}
+}
+declare module 'nuxt/schema' {
+  interface RuntimeConfig extends SharedRuntimeConfig {}
+  interface PublicRuntimeConfig extends SharedPublicRuntimeConfig {}
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: UserRuntimeConfig
+        }
+      }
