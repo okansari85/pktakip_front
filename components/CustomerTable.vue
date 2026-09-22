856da974e8353e03bf107379dcc38dd04edce523
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 17:31:39 +0300

    Expose expert impersonation action

--- components/CustomerTable.vue
+++ components/CustomerTable.vue
@@ -1,3 +1,5 @@
-<template><CustomerTableImpl v-bind="$attrs" @open="$emit('open', $event)" /></template>
-<script setup lang="ts">import CustomerTableImpl from './admin/CustomerTable.vue'
-defineEmits<{ open: [tenant: any] }>()</script>
+<template><CustomerTableImpl v-bind="$attrs" @open="$emit('open', $event)" @impersonate="$emit('impersonate', $event)" /></template>
+<script setup lang="ts">
+import CustomerTableImpl from './admin/CustomerTable.vue'
+defineEmits<{ open: [tenant: any]; impersonate: [tenant: any] }>()
+</script>
