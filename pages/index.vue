266483ceb1afe8120e207d3304f42c33674cb261
OKAN SARI <sari_okan@hotmail.com>
2026-09-23 00:14:54 +0300

    Route authenticated users to their correct panel

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,14 @@
-<script setup lang="ts">const { loadStoredAuth, token, user } = useAuth();loadStoredAuth();if (token.value && user.value?.roles?.includes('super-admin')) { await navigateTo('/customers') } else { await navigateTo('/login') }</script>
+<script setup lang="ts">
+const { loadStoredAuth, token, user } = useAuth()
+loadStoredAuth()
+
+if (token.value && user.value?.is_expert) {
+  await navigateTo('/expert')
+} else if (token.value && user.value?.roles?.includes('super-admin')) {
+  await navigateTo('/customers')
+} else {
+  await navigateTo('/login')
+}
+</script>
+
 <template><div /></template>
